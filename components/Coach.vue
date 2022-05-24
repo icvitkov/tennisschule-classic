4824eb0850bc7c7426a4276a35e4d739750a6b17
icvitkov <ivana.cvit@hotmail.com>
2022-05-24 07:42:45 +0200

    prepare component for coach info, add main section

---
+++ components/Coach.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="coach">
+        <div class="grid">
+            <h2>Coach Oliver</h2>
+        </div>
+    </section>
+</template>
+
+<style lang='scss' scoped>
+    .coach {
+        padding-block: 40px;
+    }
+</style>
---
+++ components/TsMain.vue
@@ -0,0 +1,48 @@
+<script setup>
+import TsNavbar from './shared/TsNavbar.vue'
+</script>
+
+<template>
+  <section class="main">
+    <TsNavbar />
+    <div class="block"></div>
+    <img src="assets/images/tennisPlayer.png" alt="" class="main__img" />
+    <div class="blocky--right"></div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  height: 100vh;
+  display: grid;
+  overflow: hidden;
+
+  &__img {
+    height: 90vh;
+    justify-self: center;
+    align-self: end;
+  }
+}
+.block {
+  background-color: rgba(31, 33, 48, 0.801);
+  width: 60%;
+  height: 100%;
+  border-radius: 5px;
+  transform: skew(35deg);
+  position: absolute;
+  opacity: 0.5;
+  z-index: -1;
+  left: -25%;
+}
+
+.blocky--right {
+  background-color: #cedc00;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  transform: skew(-55deg);
+  position: absolute;
+  z-index: -1;
+  left: 65%;
+}
+</style>
