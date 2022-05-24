1ec2f3c10c2e9403160bfa1e9b0a1dad24c94348
icvitkov <ivana.cvit@hotmail.com>
2022-05-24 07:41:50 +0200

    create component and section for showing hotel, courts and isle

---
+++ components/Advantages.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import TsAdvantages from './shared/TsAdvantages.vue';
+</script>
+
+<template>
+<div class="advantages">
+    <div class="grid">
+        <TsAdvantages title="Tenis u Bretanide Sport & Wellness Resortu" img="assets/images/court.jpg" class="advantages__item">
+            <template #description>
+                <p>Odmah do hotela nalazi se 5 lijepo smještenih zemljanih teniskih terena. Hannes Zischka Sportreisen nudi sate tenisa za sve razine više od 25 godina. Popularni tjedni kampovi na programu su u proljeće i jesen, a tijekom ljetnih mjeseci ( 15.6.- 31.8.) satovi tenisa se rezerviraju i organiziraju individualno na licu mjesta u Tenis školi Oliver Janković. Bretanide Sport & Wellness Resort također je vrlo popularno mjesto za grupe, klubove i škole tenisa za nekoliko teniskih dana uz more.</p>
+            </template>
+        </TsAdvantages>
+
+        <TsAdvantages title="Hotel Bretanide" img="assets/images/betanide.jpg" class="advantages__item">
+            <template #description>
+                <p>Odmah do hotela nalazi se 5 lijepo smještenih zemljanih teniskih terena. Hannes Zischka Sportreisen nudi sate tenisa za sve razine više od 25 godina. Popularni tjedni kampovi na programu su u proljeće i jesen, a tijekom ljetnih mjeseci ( 15.6.- 31.8.) satovi tenisa se rezerviraju i organiziraju individualno na licu mjesta u Tenis školi Oliver Janković. Bretanide Sport & Wellness Resort također je vrlo popularno mjesto za grupe, klubove i škole tenisa za nekoliko teniskih dana uz more.</p>
+            </template>
+        </TsAdvantages>
+        
+        <TsAdvantages title="Otok Brač" img="assets/images/brac.jpg" class="advantages__item">
+            <template #description>
+                <p>Odmah do hotela nalazi se 5 lijepo smještenih zemljanih teniskih terena. Hannes Zischka Sportreisen nudi sate tenisa za sve razine više od 25 godina. Popularni tjedni kampovi na programu su u proljeće i jesen, a tijekom ljetnih mjeseci ( 15.6.- 31.8.) satovi tenisa se rezerviraju i organiziraju individualno na licu mjesta u Tenis školi Oliver Janković. Bretanide Sport & Wellness Resort također je vrlo popularno mjesto za grupe, klubove i škole tenisa za nekoliko teniskih dana uz more.</p>
+            </template>
+        </TsAdvantages>
+    </div>
+</div>
+</template>
+
+<style lang='scss' scoped>
+.advantages {
+    background-color: #eee;
+    color: var(--color-gray);
+    padding-block: 40px;
+
+    &__item {
+        grid-column: 1/-1;
+    }
+}
+</style>
---
+++ components/shared/TsAdvantages.vue
@@ -0,0 +1,73 @@
+<script setup>
+    defineProps({
+        title: '',
+        img: '',
+    })
+</script>
+
+<template>
+<div class="card">
+    <div class="grid">
+            <div class="container"><img :src="img" alt="" class="img"></div>
+            <div class="description">
+                <h2 class="title">{{title}}</h2>
+                <slot name="description"></slot>
+            </div>
+        </div>
+</div>
+</template>
+
+<style lang='scss' scoped>
+    .card {
+        padding-block: 40px;
+    }
+
+    .img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        position: relative;
+        left: 30px;
+        top: 30px;
+  /*       filter: grayscale(1);
+        transition: all ease 0.3s;
+
+        :hover {
+            filter: none;
+        } */
+    }
+
+    .container{
+        height: 400px;
+        width: 100%;
+        background: var(--color-lime);
+        grid-column: 1/5;
+
+        :hover {
+            filter: none;
+        }
+
+    }
+
+    .description {
+        grid-column: 6/12;
+    }
+
+    .title {
+        font-size: 32px;
+        font-family: var(--font-secondary);
+        padding-bottom: 28px;
+
+           &::after {
+            content: "";
+            background-color: var(--color-lime);
+            width: 150px;
+            height: 3px;
+            border-radius: 5px;
+            margin-top: 45px;
+            margin-left: -140px;
+            position: absolute;
+                }
+    }
+    
+</style>
