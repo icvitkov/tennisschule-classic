744d9e94a3522d70033354db78ab8cb03df3dac5
icvitkov <ivana.cvit@hotmail.com>
2022-05-24 07:23:27 +0200

    add navbar boilerplate

---
+++ components/shared/TsNavbar.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+<template>
+  <div class="grid">
+    <nav class="nav">
+      <div>logo</div>
+      <div class="nav__items">
+          <div>about us</div>
+          <div>prices</div>
+          <div>contact</div>
+      </div>
+    </nav>
+  </div>
+</template>
+<style scoped lang="scss">
+.nav {
+    grid-column: 1/-1;
+    display: flex;
+    justify-content: space-between;
+
+    &__items {
+        display: flex;
+        gap: 20px;
+    }
+}
+</style>
